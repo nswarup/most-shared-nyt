59e304d7bacf79a6c42b6506fe4df7bd9f594b31
Lahsiv <v.talasani@gmail.com>
2016-12-15 22:45:15 -0500

    Short Urls, Cookies
    Changed the tweets to have shorter urls, fixed some network issues by adding cookie capability and adding some time delays.

--- nyt.py
+++ nyt.py
@@ -3,23 +3,28 @@ import json
 import twitter
 import pickle
 import os.path
+from time import sleep
 
 def get_recent_nyts_articles():
     # gets a list of recent NYT articles
-    url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json?api-key=API_KEY"
-    js = json.loads(urllib2.urlopen(url).read())
+    url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json?api-key=KEY"
+    sleep(1)
+    response = urllib2.urlopen(url).read()
+    js = json.loads(response)
     out = []
     for article in js['results']:
         out.append(article['url'])
     return out
 
 def get_popularities(links):
-    # Gets the FB share+like count for each of the links in link.
-    # links is an array of urls of articles
-    # returns an array of (share+like) counts as well as descriptions of the articles
+    '''
+    Gets the FB share+like count for each of the links in link.
+    :param links: Array of urls of articles
+    :return: A corresponding array of (share+like) counts as well as descriptions of the articles
+    '''
     out = []
     for link in links:
-        fb_url = "https://graph.facebook.com/v2.8/" + link + "?access_token=ACC_TOKEN"
+        fb_url = "https://graph.facebook.com/v2.8/" + link + "?access_token=TOKEN"
         s = urllib2.urlopen(fb_url).read()
         js = json.loads(s)
         descrip = js['og_object']['title']
@@ -37,28 +42,34 @@ def get_all_tweets():
         already_tweeted = set()
     else:
         already_tweeted = pickle.load(open("already_tweeted_articles.p"))
+    popularities.sort()
     for p in popularities:
         if p[2] in already_tweeted:
             continue
         else:
             if p[0] > 50000:
-                out.append(p)
+                out.append((p[1], get_short_url(p[2])))
                 already_tweeted.add(p[2])
     pickle.dump(already_tweeted, open("already_tweeted_articles.p", "w"))
     return out
+
+def get_short_url(u):
+    # gets the NYT short url from a given long url to an article
+    html = urllib2.build_opener(urllib2.HTTPCookieProcessor).open(u).read()
+    short_url = html.split("<span class=\"story-short-url\"><a href=\"")[1].split("\">")[0]
+    return short_url
 
 def send_tweets(tweets):
     # sends the tweets in the list "tweets"
     api = twitter.Api(consumer_key='KEY',
                       consumer_secret='SECRET',
-                      access_token_key='ACESSS',
+                      access_token_key='ACCESS',
                       access_token_secret='SECRET')
     for tweet in tweets:
-        text_to_post = tweet[2] + " " + tweet[1]
+        text_to_post = tweet[0] + " " + tweet[1]
         print text_to_post
         #api.PostUpdate()
 
+
 tweets = get_all_tweets()
 send_tweets(tweets)
-
-
